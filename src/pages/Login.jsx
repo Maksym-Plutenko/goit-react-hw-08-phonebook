16f334f6de20e8307470774fdf20c4d658e6ca52
Maksym-Plutenko <plutenkom@gmail.com>
2023-04-19 23:27:53 +0300

    redirecting part 1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,20 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { logIn } from 'redux/authorization/operations';
 
 import css from './index.module.css';
 
+import { useNavigate,  Navigate} from 'react-router-dom';
+import { selectIsLoggedIn } from 'redux/authorization/selectors';
+
 const Login = () => {
   const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+  // const isLoggedIn = useSelector(selectIsLoggedIn);
+  // if (isLoggedIn) {
+  //   navigate('/contacts', { replace: true });
+  // }
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -21,6 +30,7 @@ const Login = () => {
 
   return (
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+      <Navigate to='/contacts'/>
       <label className={css.label}>
         Email
         <input type="email" name="email" />
