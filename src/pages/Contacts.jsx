f3e758ecc3e7c307776bd520a85f751e2e0cc371
Maksym-Plutenko <plutenkom@gmail.com>
2023-04-25 13:52:45 +0300

    contacts bugfix

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,25 +1,48 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
 import {ContactForm} from '../components/ContactForm/ContactForm';
 import {Filter} from '../components/Filter/Filter';
 import {ContactList} from '../components/ContactList/ContactList';
 
+import { selectIsLoggedIn } from 'redux/authorization/selectors';
+
 const Contacts = () => {
+
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+
     return (
-           <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactList />
-    </div>
+
+      <>
+      {isLoggedIn ? (
+        <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+        >
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+        
+        </div>
+        
+      ) : (
+        <Navigate to="/" />
+      )}
+    </>
+
+
+
+
+
     );
 }
 
