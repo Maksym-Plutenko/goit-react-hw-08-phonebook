66b41179927df236625089e525d78d251ac1457f
Maksym-Plutenko <plutenkom@gmail.com>
2023-05-17 14:19:33 +0300

    auth/refresh bugfix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,62 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-
-// import { rootReducer } from './rootReducer';
-
-// const store = configureStore({
-//   reducer: rootReducer,
-// });
-
-// export { store };
-
-
-// import { configureStore } from '@reduxjs/toolkit';
-
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-
-// import { rootReducer } from './rootReducer';
-
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   whitelist: ['token'],
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// const persistor = persistStore(store);
-
-// export { store, persistor };
-
-
-
-
-
-
-
-
-
-
 import { configureStore } from '@reduxjs/toolkit';
 
 import {
@@ -71,27 +12,15 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import {contactsReduser} from './tasks/contactsSlice';
-import {filterReduser} from './tasks/filterSlice';
-import {authReducer} from './authorization/authSlice';
+import { contactsReduser } from './tasks/contactsSlice';
+import { filterReduser } from './tasks/filterSlice';
+import { authReducer } from './authorization/authSlice';
 
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: ['token'],
 };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
 
 const store = configureStore({
   reducer: {
